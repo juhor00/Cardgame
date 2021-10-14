5091a2a3950e75178da77a50a1ed321ea24374f4
juhor <juho.rantala@live.com>
2021-10-14 23:07:18 +0300

    Turn widget background color added

--- client/gui/gamewindow/turn.py
+++ client/gui/gamewindow/turn.py
@@ -5,9 +5,8 @@ from PIL import Image, ImageTk
 class Turn(Label):
 
     def __init__(self, parent):
-        super().__init__(parent, bd=0)
+        super().__init__(parent, bd=0, bg=parent["bg"])
         self.parent = parent
-
         image = Image.open("gui/gamewindow/assets/your_turn.png")
         image = image.resize((232, 113), Image.ANTIALIAS)
         self.image = ImageTk.PhotoImage(image)
