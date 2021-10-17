9375f0f58e223be7595653eb35d1414b2580f955
juhor <juho.rantala@live.com>
2021-10-18 00:58:30 +0300

    Fixed claim send bugs on server side

--- server/eventhandler.py
+++ server/eventhandler.py
@@ -202,8 +202,18 @@ class EventHandler:
         # Claim data
         claim_rank = self.game.gamedeck.get_last_rank()
         claim_amount = self.game.gamedeck.get_last_amount()
-        claim_data = {"game": {"latest": {"amount": claim_amount, "rank": claim_rank,
-                                          "id": self.claim_id, "name": str(self.game.get_last_played_player()),
+
+        # Get player name
+        player = self.game.get_last_played_player()
+        if not player:
+            name = ""
+        else:
+            name = self.game.get_last_played_player().get_name()
+
+        claim_data = {"game": {"latest": {"amount": claim_amount,
+                                          "rank": claim_rank,
+                                          "id": self.claim_id,
+                                          "name": name,
                                           "deck": self.game.last_round_played_deck()},
                                "duration": None,
                                "display": []}}
